cc41958ee050224b39e001cbee78bfd8acc08793
iamthakur9648 <iamthakur9648@gmail.com>
2026-08-15 22:00:06 +0530

    Story website with php

--- Chapter_33.php
+++ Chapter_33.php
@@ -3,7 +3,7 @@
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8" />
-    <title>THE MAN, THE BOY AND THE DONKEY</title>
+    <title>TREES THAT WOOD</title>
     <link rel="stylesheet" href="Style_2.css" />
 </head>
 
